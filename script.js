329dd198fd08068a5eae89b702bfcaef0d7602c4
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-06-04 16:04:40 -0400

    Added randompassword generator to signup match. Also made a condition to make sure that password and confirmpassword match

--- script.js
+++ script.js
@@ -14,8 +14,34 @@ function signupClick(){
 function backtoLogin(){
     document.getElementById("signUpContent").style.display = "none" 
     document.getElementById("formContent").style.display = "inline" 
+    var firstPassword = document.getElementById("password").value
+    var confirmPassword = document.getElementById("confirmPassword").value
 
 
+
+    if (firstPassword === confirmPassword) {
+  
+      }else {
+        alert("Passwords don't match. Please try again.");
+      }
+  
+
+
+}
+
+
+
+function randomPassword(event) {
+    event.preventDefault()
+
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+    var finalpassword = '';
+    for ( var i = 0; i < 12; i++ ) {
+        finalpassword += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    console.log(finalpassword)
+    document.getElementById("password").value = finalpassword
+    document.getElementById("confirmPassword").value = finalpassword
 }
 
 
