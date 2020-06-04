c6dd4cc9fd46f824ff793366d59f6f14819a3f26
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-06-04 14:18:04 -0400

    Added Js file and js functions to toggle between login and sign up page

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function signupClick(){
+    document.getElementById("formContent").style.display = "none" 
+    document.getElementById("signUpContent").style.display = "inline"
+    
+   
+    
+
+}
+
+
+
+function backtoLogin(){
+    document.getElementById("signUpContent").style.display = "none" 
+    document.getElementById("formContent").style.display = "inline" 
+
+
+}
+
+
+
+    
